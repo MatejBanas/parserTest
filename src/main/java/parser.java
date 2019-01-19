0aa4baa9b89f443491fc6c8811eaeb26421fc41e
Matej Banas <xbanas@fi.muni.cz>
2019-01-19 20:34:43 +0100

    inserting PM traps to void methods

--- src/main/java/parser.java
+++ src/main/java/parser.java
@@ -31,14 +31,14 @@ public class parser {
     //private static final String FILE_PATH3 = "src\main\resources\\PMC.java";
     private static List<Byte> constants = new ArrayList<Byte>();
 
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         File file = new File(FILE_PATH);
         //File file2 = new File(FILE_PATH2);
         //File file3 = new File(FILE_PATH3);
 
         CompilationUnit compilationUnit = JavaParser.parse(file);
-       // CompilationUnit compilationUnit2 = JavaParser.parse(file2);
-       // CompilationUnit compilationUnit3 = JavaParser.parse(file3);
+        // CompilationUnit compilationUnit2 = JavaParser.parse(file2);
+        // CompilationUnit compilationUnit3 = JavaParser.parse(file3);
 
         getAllConstants(compilationUnit);
         System.out.println(getConstants());
@@ -46,7 +46,7 @@ public class parser {
         insertTraps(compilationUnit);
         insertConstant(compilationUnit);
 
-        writeChanges(FILE_PATH, compilationUnit);
+        //writeChanges(FILE_PATH, compilationUnit);
 
         System.out.println(compilationUnit);
 
@@ -56,10 +56,11 @@ public class parser {
      * triggers a visitor that finds all constants and adds them to class attribute List constants
      * they can be used later for apdu triggers
      * visitor used: ConstantVisitor
-     *?
+     * ?
+     *
      * @param compilationUnit compilationUnit created by parsing source file
      */
-    public static void getAllConstants(CompilationUnit compilationUnit){
+    public static void getAllConstants(CompilationUnit compilationUnit) {
         List<Byte> tmpConstants = new ArrayList<Byte>();
         compilationUnit.findAll(VariableDeclarator.class).stream()
                 .filter(f -> f.getType().isPrimitiveType())
@@ -73,7 +74,7 @@ public class parser {
      *
      * @param compilationUnit compilationUnit created by parsing source file
      */
-    public static void insertTraps(CompilationUnit compilationUnit){
+    public static void insertTraps(CompilationUnit compilationUnit) {
         compilationUnit.accept(new MethodAddPM(), null);
     }
 
@@ -83,16 +84,16 @@ public class parser {
      *
      * @param compilationUnit compilationUnit created by parsing source file
      */
-    public static void insertSwitchCaseStmnt(CompilationUnit compilationUnit){
+    public static void insertSwitchCaseStmnt(CompilationUnit compilationUnit) {
         compilationUnit.accept(new ProccesAddSwitch(), null);
     }
 
     /**
-     *  adds INS_PERF_SETSTOP constant from PM to source class
+     * adds INS_PERF_SETSTOP constant from PM to source class
      *
      * @param compilationUnit compilationUnit created by parsing source file
      */
-    public static void insertConstant(CompilationUnit compilationUnit){
+    public static void insertConstant(CompilationUnit compilationUnit) {
         BodyDeclaration<?> declaration = JavaParser.parseAnnotationBodyDeclaration("public final static byte INS_PERF_SETSTOP = (byte) 0xf5;");
         compilationUnit.getTypes().get(0).getMembers().addFirst(declaration);
     }
@@ -100,7 +101,7 @@ public class parser {
     /**
      * writes changes to source file
      *
-     * @param path path to source file
+     * @param path            path to source file
      * @param compilationUnit compilationUnit created by parsing source file
      * @throws IOException exception
      */
@@ -118,7 +119,7 @@ public class parser {
      * @param a compilationUnit created by parsing source file
      * @param b compilationUnit created by parsing file of which we want to change the package declaration
      */
-    public static void changePackage(CompilationUnit a, CompilationUnit b){
+    public static void changePackage(CompilationUnit a, CompilationUnit b) {
         b.setPackageDeclaration(a.getPackageDeclaration().get());
     }
 
@@ -133,13 +134,13 @@ public class parser {
     /**
      * visitor that gets all constants
      */
-    private static class ConstantVisitor extends GenericListVisitorAdapter<Byte, Void > {
+    private static class ConstantVisitor extends GenericListVisitorAdapter<Byte, Void> {
         List<Byte> bytes = new ArrayList<Byte>();
 
         @Override
-        public List<Byte> visit(VariableDeclarator variableDeclarator, Void arg){
+        public List<Byte> visit(VariableDeclarator variableDeclarator, Void arg) {
             String name = variableDeclarator.getName().asString();
-            if(variableDeclarator.getType().asString().equals("byte") && name.equals(name.toUpperCase()) ){
+            if (variableDeclarator.getType().asString().equals("byte") && name.equals(name.toUpperCase())) {
                 bytes.add((byte) variableDeclarator.getInitializer().get().asCastExpr().getExpression().asIntegerLiteralExpr().asInt());
                 //variableDeclarator.getInitializer().get().ifIntegerLiteralExpr(f -> bytes.add((byte) f.asInt()));
             }
@@ -152,23 +153,26 @@ public class parser {
      */
     private static class MethodAddPM extends ModifierVisitor<Void> {
         @Override
-        public MethodDeclaration visit(MethodDeclaration methodDeclaration, Void arg){
+        public MethodDeclaration visit(MethodDeclaration methodDeclaration, Void arg) {
             super.visit(methodDeclaration, arg);
             if (!methodDeclaration.getName().asString().matches("process") && !methodDeclaration.getName().asString().matches("install")) {
-
+                boolean returnFound = false;
                 NodeList<Statement> statements = methodDeclaration.getBody().get().getStatements();
                 BlockStmt body = new BlockStmt();
                 body.addStatement("PM.check(PMC.TRAP_methodName_0);");
-                for(int i = 0; i < statements.size(); i++){
-                    if (statements.get(i).isReturnStmt()){
+                for (int i = 0; i < statements.size(); i++) {
+                    if (statements.get(i).isReturnStmt()) {
+                        returnFound = true;
                         body.addStatement("PM.check(PMC.TRAP_methodName_0);");
                     }
                     body.addStatement(statements.get(i));
                 }
-                //body.addStatement("PM.check(PMC.TRAP_methodName_0);");
+                if (!returnFound) {
+                    body.addStatement("PM.check(PMC.TRAP_methodName_0);");
+                }
                 methodDeclaration.setBody(body);
             }
-        return methodDeclaration;
+            return methodDeclaration;
         }
     }
 
@@ -177,27 +181,27 @@ public class parser {
      */
     private static class ProccesAddSwitch extends ModifierVisitor<Void> {
         @Override
-        public MethodDeclaration visit (MethodDeclaration methodDeclaration, Void arg){
+        public MethodDeclaration visit(MethodDeclaration methodDeclaration, Void arg) {
             super.visit(methodDeclaration, arg);
-            if (methodDeclaration.getName().asString().equals("process")){
+            if (methodDeclaration.getName().asString().equals("process")) {
                 NodeList<Statement> statements = methodDeclaration.getBody().get().getStatements();
                 BlockStmt body = new BlockStmt();
                 NodeList<Statement> toBeInserted = new NodeList<Statement>();
 
-                for(int i =0; i< statements.size(); i++){
+                for (int i = 0; i < statements.size(); i++) {
                     /**
-                    if(statements.get(i).getClass().equals(ExpressionStmt.class)){
-                        System.out.println(statements.get(i).asExpressionStmt().getExpression().getClass());
-                    }
+                     if(statements.get(i).getClass().equals(ExpressionStmt.class)){
+                     System.out.println(statements.get(i).asExpressionStmt().getExpression().getClass());
+                     }
                      */
-                    if (statements.get(i).isSwitchStmt()){
+                    if (statements.get(i).isSwitchStmt()) {
                         String name = statements.get(i).asSwitchStmt().getSelector().asArrayAccessExpr().getName().toString();
-                        toBeInserted.add(JavaParser.parseStatement("PM.m_perfStop = Util.makeShort("+name+"[ISO7816.OFFSET_CDATA], "+name+"[(short) (ISO7816.OFFSET_CDATA + 1)]);"));
+                        toBeInserted.add(JavaParser.parseStatement("PM.m_perfStop = Util.makeShort(" + name + "[ISO7816.OFFSET_CDATA], " + name + "[(short) (ISO7816.OFFSET_CDATA + 1)]);"));
                         toBeInserted.add(new BreakStmt().removeLabel());
                         NodeList<SwitchEntryStmt> entryStmts = statements.get(i).asSwitchStmt().getEntries();
-                        entryStmts.addBefore(new SwitchEntryStmt(new NameExpr("INS_PERF_SETSTOP"), toBeInserted), statements.get(i).asSwitchStmt().getEntry(entryStmts.size()-1));
+                        entryStmts.addBefore(new SwitchEntryStmt(new NameExpr("INS_PERF_SETSTOP"), toBeInserted), statements.get(i).asSwitchStmt().getEntry(entryStmts.size() - 1));
                         statements.get(i).asSwitchStmt().setEntries(entryStmts);
-                        }
+                    }
                     body.addStatement(statements.get(i));
                 }
                 methodDeclaration.setBody(body);
